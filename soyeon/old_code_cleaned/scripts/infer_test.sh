72a9632982613ab02f6670af018bf61f99f7e037
kimcando <kimcando94@gmail.com>
2022-05-10 14:24:16 +0000

    adventrues..

---
+++ soyeon/old_code_cleaned/scripts/infer_test.sh
@@ -0,0 +1,24 @@
+# ODQA 실행 (test_dataset 사용)
+# wandb 가 로그인 되어있다면 자동으로 결과가 wandb 에 저장됩니다. 아니면 단순히 출력됩니다
+
+# 이 코드는 test dataset이 아닌 train dataset에 있는 validation 데이터를 이용해 
+# retrieval와 inference 성능을 평가합니다. 
+
+# Sparse Retrieval의 경우 성능이 바뀌지 않겠지만, Dense Retrieval의 경우 학습에 따라 성능이 바뀔 수 있으니 \
+# 이 코드를 먼저 돌려보시고 inference.sh를 실행하는 것을 권장합니다.
+# Sparse Retrieval의 경우에도 이 코드를 돌리면 소연님이 구현하신대로 wandb 에서 결과를 볼 수 있습니다.
+
+# ***주의*** : 해당 코드로 저장되는 predictions.json은 test dataset의 결과물이 아니기 때문에 
+#             리더보드에 제출하면 안됩니다..!(이 파일은 outputs/valid_dataset/에 저장됩니다. )
+
+python inference_validate.py \
+--output_dir ./outputs/valid_dataset/ \
+--model_name_or_path /opt/ml/level2-mrc-level2-nlp-03/soyeon/old_baseline/model_dir/checkpoint-2400 \
+--use_wandb True \
+--exp_name inference_validate \
+--do_predict False \
+--do_eval True \
+--overwrite_output_dir True \
+--add_tokens False \
+--top_k_retrieval 5
+#--dataset_name train_dataset/ \
---
+++ soyeon/old_code_cleaned/utils/postprocessing.py
@@ -0,0 +1,64 @@
+from transformers import EvalPrediction
+from typing import Callable, Dict, List, NoReturn, Tuple
+import numpy as np
+
+from trainer_qa import QuestionAnsweringTrainer, QuestionAnsweringBaseTrainer
+from utils.utils_qa import postprocess_qa_predictions
+
+# Post-processing:
+def initiate_trainer(
+        model,
+        training_args,
+        data_args,
+        train_dataset,
+        eval_dataset,
+        eval_examples,
+        tokenizer,
+        data_collator,
+        compute_metrics,
+        callbacks,
+        answer_column_name):
+
+    def post_processing_function(examples,features, predictions,training_args) -> EvalPrediction:
+        # Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
+        predictions = postprocess_qa_predictions(
+            examples=examples,
+            features=features,
+            predictions=predictions,
+            max_answer_length=data_args.max_answer_length,
+            output_dir=training_args.output_dir,
+        )
+        # Metric을 구할 수 있도록 Format을 맞춰줍니다.
+        formatted_predictions = [
+            {"id": k, "prediction_text": v} for k, v in predictions.items()
+        ]
+        print('in post processing')
+        breakpoint()
+        if training_args.do_predict:
+            return formatted_predictions
+
+        elif training_args.do_eval:
+            print(eval_dataset)
+            references = [
+                {"id": ex["id"], "answers": ex[answer_column_name]}
+                for ex in eval_examples #datasets["validation"]
+            ]
+
+            return EvalPrediction(
+                predictions=formatted_predictions, label_ids=references
+            )
+
+    trainer = QuestionAnsweringBaseTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        eval_examples=eval_examples,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=post_processing_function,
+        compute_metrics=compute_metrics,
+        callbacks=callbacks,
+    )
+
+    return trainer
